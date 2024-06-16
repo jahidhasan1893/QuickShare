10a16ae85d162b3b73e9a92fbd91a961de616f6a
Md Jahid Hasan <jahidhasan1893@gmail.com>
2024-06-16 10:55:36 +0600

    File sharing implemented

---
+++ config/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+function connectDB() {
+    // Database connection
+    mongoose.connect(process.env.MONGO_CONNECTION_URL)
+        .then(() => {
+            console.log('Database Connected!!');
+        })
+        .catch(err => {
+            console.error('Connection failed.', err);
+        });
+}
+
+
+module.exports = connectDB;
